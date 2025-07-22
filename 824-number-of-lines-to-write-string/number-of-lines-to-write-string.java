0867569242814b6b63e1240e0d866851db4bebe0
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-07-22 14:33:28 +0200

    Time: 0 ms (100.00%) | Memory: 41.4 MB (86.50%) - LeetSync

--- 824-number-of-lines-to-write-string/number-of-lines-to-write-string.java
+++ 824-number-of-lines-to-write-string/number-of-lines-to-write-string.java
@@ -4,31 +4,21 @@ class Solution {
         // String to char array
         int currWidth = 0;
         int line = 1;
-        StringBuilder str = new StringBuilder();
         for(char ch : s.toCharArray()){
         // cal index of char to find width 
             int index = ch - 'a';
-            System.out.println("Index : "+index);
-        //  add it in to currWidth
+                    //  add it in to currWidth
         // check currWidth <= 100 then add char to String
 
             if(currWidth + widths[index] > 100)
             {
                 line++;
-                currWidth =0;
-                str.setLength(0);
-            } 
-
+                currWidth = widths[index];
+            } else{
                 currWidth = currWidth + widths[index];
-                str.append(ch);       
+            }
              }
 
-           int result = 0;
-           for(char ch : str.toString().toCharArray()){
-        // cal index of char to find width 
-            int index = ch - 'a';
-            result += widths[index];
-           }
-        return new int[]{line, result};
+        return new int[]{line, currWidth};
     }
 }
