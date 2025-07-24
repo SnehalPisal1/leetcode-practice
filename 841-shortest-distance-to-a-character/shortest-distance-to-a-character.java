ccd23f30b5242b7bdffd0e860c902b7c54bd2b85
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-07-24 15:51:32 +0200

    Time: 308 ms (5.14%) | Memory: 45.4 MB (8.98%) - LeetSync

---
+++ 841-shortest-distance-to-a-character/shortest-distance-to-a-character.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int[] shortestToChar(String s, char c) {
+
+
+        System.out.println(s.length());
+
+        // String to char Array 
+        int[] arr= new int[s.length()];
+
+        List<Integer> result = new ArrayList<>();
+
+        char[] chArr = s.toCharArray();
+
+        for(int i = 0; i<chArr.length; i++){
+            System.out.println(i);
+
+            if(chArr[i] == c){
+              //add to list - > 0
+                result.add(0);
+                System.out.println(result);
+            }
+            else{
+                int minDist = Integer.MAX_VALUE;
+           //itrate loop till we found give char , count -> add it into list
+            for(int j = 0; j<chArr.length; j++){
+                if(chArr[j] == c){
+                //check char == given char c
+                int dist=Math.abs(j-i);
+                if(minDist > dist)
+                minDist = dist;
+                }   
+             } 
+              result.add(minDist);
+                System.out.println(result);
+            }
+        }
+
+        System.out.println(result);
+
+        for(int i = 0; i<arr.length; i++){
+            arr[i] = result.get(i);
+        }
+         return arr;
+    }
+}
