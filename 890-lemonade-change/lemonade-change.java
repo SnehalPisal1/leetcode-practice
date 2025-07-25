e9779ab5eb18b973f1d41d61a26ecc82ab8babeb
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-07-25 16:15:11 +0200

    Time: 2 ms (93.19%) | Memory: 55.9 MB (64.50%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,64 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+
+        int five = 0 ;
+        int ten = 0;
+
+        for(int bill : bills){
+
+            if(bill == 5){
+                five++;
+            }
+            else if(bill == 10){
+                if(five > 0){
+                five--;
+                 ten++;
+
+                } else{
+                    return false;
+                }
+            } else { // 20
+                 // 1 10 and 1 5 Or 3 5
+
+                 if(ten>0 && five>0){
+                    five--;
+                    ten--;
+                 } else if(five >=3) {
+                    five -=3;
+                 } else {
+                    return false;
+                 }
+
+            }
+
+        }
+            return true;
+
+
+         /*   int totalCollectedBill=0;
+          for(int bill : bills){
+                System.out.println("bill: " + bill);
+
+            if(bill == 5){
+                totalCollectedBill +=bill;
+            } else {
+
+                int returnBill =  bill - 5;
+                System.out.println("returnBill: " + returnBill);
+                
+                if(totalCollectedBill - returnBill >=0){
+                     totalCollectedBill = totalCollectedBill + 5 - returnBill;
+                } else {
+                    return false;
+                }
+            }
+            System.out.println("totalCollectedBill: " + totalCollectedBill);
+          }
+        return true;
+        */
+
+        
+
+    } 
+
+}
