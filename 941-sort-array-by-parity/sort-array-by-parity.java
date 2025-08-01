1b044188a1e43e9382d0afada66e77977cb7b177
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-08-01 21:57:13 +0200

    Time: 4 ms (6.10%) | Memory: 45.8 MB (5.68%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -9,22 +9,7 @@ class Solution {
         int[] result = new int[nums.length];
         int i = 0;
 
-      /*  for(int key :  map.keySet()){
-            if(map.get(key) == true){
-                result[i] = key;
-                i++;
-            }
-        }
-
-        // remaining
-            for(int key :  map.keySet()){
-            if(map.get(key) == false){
-                result[i] = key;
-                i++;
-            }
-        }
-       return result; */
-
+      // Even
        for(int n: nums){
              if(map.get(n) == true){
                 result[i] = n;
@@ -32,11 +17,8 @@ class Solution {
             }
        } 
 
-       
-        // remaining
+        // odd
         for(int n: nums){
-
-
             if(map.get(n) == false){
                 result[i] = n;
                 i++;
