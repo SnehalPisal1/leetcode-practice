e31e67976ac647a3c13c5dac627c5405373228b7
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-08-04 10:27:11 +0200

    Time: 29 ms (12.47%) | Memory: 45.9 MB (12.84%) - LeetSync

---
+++ 950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.java
@@ -0,0 +1,63 @@
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+
+        if(deck.length == 1){
+            return false;
+        }
+
+        //map - > card, count
+
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+
+        for(int card : deck){
+
+            map.put(card, map.getOrDefault(card, 0) + 1);            
+
+        }
+            System.out.println(map);
+
+        
+   /*     // check values -> values should be same for each card
+        int groupSize = map.get(deck[0]);
+        for(int key: map.keySet()){ 
+            if(groupSize != map.get(key)){
+                return false;
+            }
+        }
+
+        return true; */
+
+
+        int gcd = 0 ;
+         for(int count : map.values()){
+            System.out.println(gcd +" - "+count);
+            gcd = findGcd(gcd , count);
+
+            if(gcd == 1){
+                return false;
+            }
+         }
+
+         return true;
+
+    }
+
+    public static int findGcd(int firstNo,int secNo){
+        int smallerNo = Math.min(firstNo, secNo);
+        int largerNo = Math.max(firstNo, secNo);
+
+        while(smallerNo != 0){
+                        int temp = largerNo % smallerNo; 
+
+            System.out.println(largerNo +" - "+smallerNo +" - "+temp);
+
+
+            largerNo = smallerNo;
+
+            smallerNo = temp;
+
+        }
+
+            return largerNo;
+    }
+}
