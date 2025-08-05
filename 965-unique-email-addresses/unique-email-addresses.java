eafd07aa2cfeca4a11f016156a81eb639acac5fb
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-08-05 11:23:26 +0200

    Time: 65 ms (5.14%) | Memory: 45.7 MB (6.35%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int numUniqueEmails(String[] emails) {
+
+        // string split @ local + domainname
+        // + stop processing  and . replace with "" -> processed local
+        // append  processed local + @ domainname
+        // add it into set and count
+
+        Set<String> set = new HashSet<>();
+        for(String email : emails){
+            String splittedStr[] = email.split("@");
+            System.out.println("splittedStr[0] -> "+splittedStr[0]);
+
+            String processedLocal = "";
+    
+            for(char ch :   splittedStr[0].toCharArray()){
+                if(ch == '+'){
+                    break;
+                }  else if(ch =='.') {
+                    continue;
+                }
+                else{
+                  processedLocal +=ch;  
+                } 
+            }
+            System.out.println("local -> "+processedLocal);
+
+            set.add(processedLocal+"@"+splittedStr[1]);
+
+        }
+
+        System.out.println("Set : "+set);
+
+
+        return set.size();
+    }
+}
