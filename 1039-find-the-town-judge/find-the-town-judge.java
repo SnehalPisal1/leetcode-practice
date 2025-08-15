c31280b01aef815e0c2137e3e07e3e73f802fc01
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-08-15 16:42:02 +0200

    Time: 19 ms (7.52%) | Memory: 51.9 MB (94.87%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -2,22 +2,26 @@ import java.util.*;
 
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        Set<Integer> trusters = new HashSet<>();
-        Map<Integer, Integer> trustCount = new HashMap<>();
+       
+       // trust - > i -> truster
+       // trust - > j -> may be judge
 
-        for (int[] relation : trust) {
-            int a = relation[0];
-            int b = relation[1];
-            trusters.add(a);
-            trustCount.put(b, trustCount.getOrDefault(b, 0) + 1);
+       Set<Integer>  truster = new HashSet<>();
+       Map<Integer, Integer> count = new HashMap<>();
+
+       for(int[] relation : trust){
+        int a = relation[0];
+        int b = relation[1];
+        truster.add(a);
+        count.put(b,count.getOrDefault(b,0)+1);
+       }
+
+       for(int i=1; i<=n ;i++){
+        if(!truster.contains(i) && count.getOrDefault(i, 0) == n-1){
+            return i;
         }
+       }
 
-        for (int i = 1; i <= n; i++) {
-            if (!trusters.contains(i) && trustCount.getOrDefault(i, 0) == n - 1) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
+       return -1;
+}
 }
