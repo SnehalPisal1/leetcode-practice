79e314c266e22c86ef6d94e7de5dc6fc029bb2f1
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-08-22 09:40:26 +0200

    Time: 3 ms (44.56%) | Memory: 45.5 MB (5.92%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.java
+++ 1168-duplicate-zeros/duplicate-zeros.java
@@ -1,23 +1,20 @@
 class Solution {
     public void duplicateZeros(int[] arr) {
 
-        // if zero found -> shift all elements to right and add one zero next to zero
+        // list - > if zero found add one more zero
+        // put list values in arr
 
-        List<Integer> list= new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
 
-        for(int n: arr){
-
+        for(int n : arr){
             list.add(n);
-            if(n==0){
-                list.add(0);
+            if(n == 0){
+                list.add(n);
             }
-        } 
-        
-        for(int i=0; i < arr.length; i++){
+        }
 
-            if(i<list.size())
-            arr[i]= list.get(i);
-
+        for(int i=0 ;i < arr.length;i++){
+            arr[i] = list.get(i);
         }
     }
 }
