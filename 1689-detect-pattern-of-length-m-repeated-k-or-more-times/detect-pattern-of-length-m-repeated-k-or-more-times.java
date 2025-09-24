ac2c1ab78d795e804a13dd413d086a1c000b8ddc
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-09-24 15:28:14 +0200

    Time: 26 ms (5.81%) | Memory: 42.7 MB (6.40%) - LeetSync

--- 1689-detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.java
+++ 1689-detect-pattern-of-length-m-repeated-k-or-more-times/detect-pattern-of-length-m-repeated-k-or-more-times.java
@@ -2,32 +2,22 @@ class Solution {
     public boolean containsPattern(int[] arr, int m, int k) {
         int n = arr.length;
         
-        // If the pattern length * repetitions exceeds array length, it's impossible
-        if (m * k > n) {
-            return false;
-        }
-        
-        // Check each possible starting position for the pattern
-        for (int i = 0; i <= n - m * k; i++) {
-            boolean found = true;
-            
-            // Check if the pattern starting at i repeats k times
-            for (int j = 0; j < m; j++) {
-                // Compare each position in the pattern across k repetitions
-                for (int rep = 1; rep < k; rep++) {
-                    if (arr[i + j] != arr[i + j + rep * m]) {
-                        found = false;
-                        break;
-                    }
-                }
-                if (!found) break;
+        // n - m // 8 // 8 -2  =  6
+        int count = 0;
+        for(int i =0 ; i < n - m ;i ++){
+
+        System.out.println(arr[i] +" : "+ arr[i+m]);
+
+            if(arr[i] == arr[i+m]){
+                count++;
+            }else {
+                count= 0;
             }
-            
-            if (found) {
-                return true;
-            }
+            System.out.println("Count : "+count);
+        if(count >= m*(k-1)){
+            return true;
         }
-        
+        }
         return false;
     }
 }
