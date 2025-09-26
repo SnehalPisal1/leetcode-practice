a3ebbf50b1e212eb7a74600d04a980e49973d9a4
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-09-26 10:05:45 +0200

    Time: 0 ms (100.00%) | Memory: 41.3 MB (63.19%) - LeetSync

--- 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/sum-of-all-odd-length-subarrays.java
@@ -1,27 +1,29 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
 
+        // Mathmetical way -> count of all possible odd sequences.. by selecting possible start and end elements
+
         int totalSum = 0;
-
         int n = arr.length;
+        for(int i = 0; i<n ;i++){
 
-        for(int  i =0 ; i<n ;i++){
+            // select start and end 
 
-                int left = i + 1; // 1
-                int right = n - i; // 5
+            int left = i+1; // 1 - > start - > index 0 (1)
+            int right = n - i; // 5 - > index 0 1 2 3 4 
 
-                int totalsubArrs = left * right ;// 5
+            int totalSubArrays = left * right; // 1 * 5 -> 5
 
-                int oddsubArrs = (totalsubArrs + 1) / 2; // 3
+            int oddSubArrays = (totalSubArrays  + 1 )/ 2 ; // 5 /2 - > 2
 
-                totalSum +=oddsubArrs * arr[i];
+            totalSum += oddSubArrays * arr[i];
 
 
         }
 
-
-
         return totalSum;
+
+   
     }
         
     }
