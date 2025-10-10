c3131026f4d9c51c88667a4744bdfb6f1f403604
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-10-10 10:53:01 +0200

    Time: 263 ms (4.23%) | Memory: 45.5 MB (7.84%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.java
+++ 1755-defuse-the-bomb/defuse-the-bomb.java
@@ -1,39 +1,40 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
 
-        int n = code.length;
+        int n= code.length;
 
-        int[] result =  new int[n];
-
+        int[] result = new int[n];
         if(k == 0){
             return result;
         }
 
-        for(int i = 0 ; i<n ;i++){
-
-            int sum = 0;
+        for(int i = 0 ; i < n ;i++){
 
             if(k > 0){
-                for(int j = 1; j <= k ;j++){
-                    sum += code[(i + j) % n];
-                    System.out.println("sum : "+sum);
+
+                for(int j = 1 ; j <= k ;j++){
+
+                    result[i] += code[(j + i) % n];
                 }
 
             } else{
-                  System.out.println("inside negative : ");
 
-                for(int j = -1; j >=k; j--){
-                    int index = (i+j+n) % n;
-                      System.out.println("index : "+index);
+                System.out.println("negative : ");
 
-                    sum += code[index];
-                    System.out.println("sum : "+sum);
-                }
+             for(int j = -1 ; j >= k ; j--){
+
+                System.out.println("Index : "+(i + j + n) % n);
+
+                result[i] += code[ (i + j + n) % n]; // 3 2
+                System.out.println(result[i]);
+
+             }
+
+
+
             }
-
-            result[i] = sum;
         }
 
         return result;
-    }}
-     
+    }
+}
