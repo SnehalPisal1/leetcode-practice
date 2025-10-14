cc4637ed69d5619291a90b900a3702f3b61e5ec7
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-10-14 10:22:23 +0200

    Time: 203 ms (1.41%) | Memory: 45.9 MB (53.01%) - LeetSync

---
+++ 1839-decode-xored-array/decode-xored-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+
+        int n = encoded.length;
+        int[] result = new int[n+1];
+
+        result[0] = first;
+
+        for(int i = 0 ; i < n ; i++){
+
+            result[i+1] =  encoded[i] ^ result[i];
+
+
+            System.out.println(result[i+1]);
+            
+        }
+        
+        return result;
+    }
+}
