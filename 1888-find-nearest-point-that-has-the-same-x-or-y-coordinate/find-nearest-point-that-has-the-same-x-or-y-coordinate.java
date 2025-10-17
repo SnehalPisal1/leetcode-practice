45a69a467a0bebb1918bbc0daa12233ace638f6e
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-10-17 10:16:58 +0200

    Time: 10 ms (3.48%) | Memory: 49.6 MB (54.35%) - LeetSync

--- 1888-find-nearest-point-that-has-the-same-x-or-y-coordinate/find-nearest-point-that-has-the-same-x-or-y-coordinate.java
+++ 1888-find-nearest-point-that-has-the-same-x-or-y-coordinate/find-nearest-point-that-has-the-same-x-or-y-coordinate.java
@@ -2,30 +2,29 @@ class Solution {
     public int nearestValidPoint(int x, int y, int[][] points) {
 
         int smallestDist = Integer.MAX_VALUE;
-
         int smallestIndex = -1;
-
-        int index = 0;
+        int index=0;
         for(int[] point : points){
 
+            // valid point
             if(point[0] == x || point[1] == y){
 
-                int manhattanDist = Math.abs(x - point[0]) +  Math.abs(y - point[1]);
+                int manDist = Math.abs(x - point[0]) + Math.abs(y - point[1]);
 
-                System.out.println("manh : "+manhattanDist);
+                System.out.println("manDist : "+ manDist);
+                if(manDist < smallestDist){
 
-                if(smallestDist > manhattanDist){
-                smallestDist =  manhattanDist;
-                smallestIndex = index;
-                
-                System.out.println("Smallest : "+smallestIndex);
-
+                    smallestDist = manDist;
+                    System.out.println("smallestDist : "+ smallestDist);
+                    System.out.println("index : "+ index);
+                    smallestIndex = index;
                 }
+
             }
-            index++;
+                index++;
+
         }
 
-        return smallestIndex;
-        
+      return smallestIndex;
     }
 }
