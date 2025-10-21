490e66ae4a822f6b38ef966f2090d26721ced8ec
Snehal Pisal <snehal.pisal1005@gmail.com>
2025-10-21 11:10:14 +0200

    Time: 146 ms (8.72%) | Memory: 45.4 MB (19.21%) - LeetSync

--- 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
+++ 1938-minimum-operations-to-make-the-array-increasing/minimum-operations-to-make-the-array-increasing.java
@@ -1,25 +1,27 @@
 class Solution {
     public int minOperations(int[] nums) {
-        // If array has only one element, it's already strictly increasing
-        if (nums.length == 1) {
-            return 0;
-        }
-        
-        int operations = 0;
-        
-        // Start from the second element
-        for (int i = 1; i < nums.length; i++) {
-            // If current element is not greater than previous
-            if (nums[i] <= nums[i - 1]) {
-                // Calculate how much we need to increment
-                int needed = nums[i - 1] + 1;
-                int increment = needed - nums[i];
-                operations += increment;
-                // Update current element to the needed value
-                nums[i] = needed;
+
+        int count = 0;
+        int length =  nums.length;
+
+       for(int i = 1 ; i < length;i++){
+
+            if(nums[i] <= nums[i-1]){
+
+                int increment = nums[i - 1] + 1;
+
+                count += increment - nums[i];
+
+                nums[i] = increment;
+
+                System.out.println(i + " : " +nums[i]);
+
             }
-        }
+
+
+       }
+
+      return count; 
         
-        return operations;
     }
 }
